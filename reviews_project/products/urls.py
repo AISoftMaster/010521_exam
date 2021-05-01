920b961af6b876152ad75d05bb32d8fc3c2a6faf
ormon <ormonkenenbaev@gmail.com>
2021-05-01 18:50:30 +0600

    50 min

--- reviews_project/products/urls.py
+++ reviews_project/products/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from .views import ProductListView, ProductDetailView, ProductCreateViews, ProductUpdateView, ProductDeleteView, \
-    ReviewCreate
+from .views import ProductListView, ProductDetailView, ProductCreateViews, ProductUpdateView, \
+    ProductDeleteView
+    # ReviewCreate
 
 app_name = 'products'
 
@@ -11,5 +12,5 @@ urlpatterns = [
     path('create/', ProductCreateViews.as_view(), name='create'),
     path('<int:pk>/update/', ProductUpdateView.as_view(), name='update'),
     path('<int:pk>/delete/', ProductDeleteView.as_view(), name='delete'),
-    path('<int:pk>/reviews/<int:pk>/', ReviewCreate.as_view(), name='create_review'),
+    # path('<int:pk>/reviews/<int:pk>/', ReviewCreate.as_view(), name='create_review'),
 ]
